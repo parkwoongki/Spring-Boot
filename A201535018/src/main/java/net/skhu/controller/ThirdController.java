25343844a7f4135c2c196e888e53641de45f2f66
parkwoongki <sbe03005@naver.com>
2020-09-24 16:53:57 +0900

    과제2

--- A201535018/src/main/java/net/skhu/controller/ThirdController.java
+++ A201535018/src/main/java/net/skhu/controller/ThirdController.java
@@ -2,6 +2,8 @@ package net.skhu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.skhu.dto.Student;
@@ -15,5 +17,17 @@ public class ThirdController {
 		model.addAttribute("student", new Student(1, "201535018", "박웅기", "sbe03005@naver.com"));
 
 		return "third/test1";
+	}
+
+	@GetMapping("test2")
+	public String test2(Model model) {
+		model.addAttribute("student", new Student(0, "", "", ""));
+		return "third/test2";
+	}
+
+	@PostMapping("test2")
+	public String test2(Model model, Student student) {
+		model.addAttribute("message", "저장되었습니다");
+		return "third/test2";
 	}
 }
