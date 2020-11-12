72af0592571a5d7c0d66f490dda0fae44c91c482
parkwoongki <sbe03005@naver.com>
2020-11-12 23:59:54 +0900

    resultMap3

---
+++ resultMap3/src/main/java/net/skhu/dto/Professor.java
@@ -0,0 +1,13 @@
+package net.skhu.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Professor {
+    int id;
+    String professorName;
+    int departmentId;
+    List<Course> courses;
+}
