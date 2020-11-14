9fb75d0c222f6f7538a897448c0ce9652e989b4c
parkwoongki <sbe03005@naver.com>
2020-11-15 01:19:09 +0900

    왜 resultMap을 쓰는가4

---
+++ resultMap4/src/main/java/net/skhu/controller/ProfessorController.java
@@ -0,0 +1,25 @@
+package net.skhu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import net.skhu.dto.Professor;
+import net.skhu.mapper.ProfessorMapper;
+
+@Controller
+public class ProfessorController {
+    @Autowired
+    ProfessorMapper professorMapper;
+
+    @RequestMapping("professor/list")
+    public String list(Model model) {
+        List<Professor> professors = professorMapper.findAll();
+
+        model.addAttribute("professors", professors);
+
+        return "professor/list";
+    }
+}
